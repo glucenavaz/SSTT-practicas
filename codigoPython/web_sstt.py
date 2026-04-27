8c5dfaf565e1713bf13609139190b72557db3bc4
EduTerryGavila <eduardo.terryg@um.es>
2026-04-27 18:27:48 +0200

    cambios

--- codigoPython/web_sstt.py
+++ codigoPython/web_sstt.py
@@ -145,11 +145,11 @@ def process_web_request(cs, webroot):
                 ruta_base, params = url.split("?",1)
                 url = ruta_base # Nos quedamos con la ruta limpia para buscar el fichero
                 params = params.replace("%40", "@")
-                regex_correo = r"email=[a-z]([a-z.]*[a-z])?@organizacion6535\.org"
+                regex_correo = r"email=[a-z]([a-z.]*[a-z])?@nombreorganizacion6535\.org"
                 if re.search(regex_correo, params):
                     mensaje_email = b"<h1>Correo Correcto</h1><p>Bienvenido estudiante.</p>"
                 elif "email=" in params: # Si mandó el parámetro email pero no cumple el patrón:
-                    mensaje_email = b"<h1>Correo Erroneo</h1><p>El correo debe ser valido, en minusculas y pertenecer al dominio @organizacion6535.org.</p>"
+                    mensaje_email = b"<h1>Correo Erroneo</h1><p>El correo debe ser valido, en minusculas y pertenecer al dominio @nombreorganizacion6535.org.</p>"
             
             # Comprobar si el recurso solicitado es /, en ese caso el recurso es index.html
             if url == "/":
